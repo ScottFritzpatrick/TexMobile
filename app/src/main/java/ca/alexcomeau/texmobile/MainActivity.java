047cfa8d0379682bfeb3e94532b2b9a18b908b11
alexcomeau <erzz197+gh@gmail.com>
2015-11-25 12:43:05 -0400

    Wired up main buttons
    The buttons in the start menu now fire the appropriate intent.

--- app/src/main/java/ca/alexcomeau/texmobile/MainActivity.java
+++ app/src/main/java/ca/alexcomeau/texmobile/MainActivity.java
@@ -1,17 +1,29 @@
 package ca.alexcomeau.texmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Bundle the database if it isn't already.
         DatabaseBundler dbBundle = new DatabaseBundler(getPackageName(), getBaseContext());
         dbBundle.bundle("scoresdb");
+    }
+
+    public void btnClick(View v)
+    {
+        Intent intent = new Intent("ca.alexcomeau.texmobile.GameActivity");
+        //TODO: Add a drop down or something to select the starting level.
+        intent.putExtra("startLevel", 0);
+        intent.putExtra("maxLevel", Integer.parseInt(v.getTag().toString()));
+        startActivity(intent);
     }
 }
